a08129fc7618847105c52514946740cf3bcaecf3
mseidl-tgm <mseidl@student.tgm.ac.at>
2016-12-04 15:51:28 +0100

    pc commit

---
+++ SEW/SEW_eclipse_workspace/SEWA05_Seidl/src/view/G_API_Frame.java
@@ -0,0 +1,57 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+
+public class G_API_Frame extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					G_API_Frame frame = new G_API_Frame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public G_API_Frame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+		
+		textField_2 = new JTextField();
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_1 = new JTextField();
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+	}
+
+}
---
+++ SEW/SEW_eclipse_workspace/SEWA05_Seidl/src/view/G_API_Panel.java
@@ -0,0 +1,14 @@
+package view;
+
+import javax.swing.JPanel;
+
+public class G_API_Panel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public G_API_Panel() {
+
+	}
+
+}
